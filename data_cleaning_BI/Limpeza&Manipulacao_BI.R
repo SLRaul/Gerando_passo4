ddb92b79f36c4efa08b967b623805c2c10315a71
Raul <silva.raull>
2021-01-20 15:06:53 -0300

    update de colunas

--- data_cleaning_BI/Limpeza&Manipulacao_BI.R
+++ data_cleaning_BI/Limpeza&Manipulacao_BI.R
@@ -3,6 +3,7 @@ rm(list = ls())
 library(dplyr)
 library(readxl)
 library(openxlsx)
+library(dplyr)
 
 # lendo a lista de tabelas e unido-as em uma
 # list.files() -> copia todos os arquivos(do padrão escolhido) da pasta que o caminho leva
@@ -19,6 +20,8 @@ Mes <- rep(c(04,08,12,02,01,07,06,05,03,11,10,09), each=37)
 # juntando o mes a tabela final
 list_table_concil <- cbind(list_table_concil, Mes )
 
+list_table_concil <- list_table_concil %>% select(- `Descrição da Região Judiciária`)
+
 write.xlsx(list_table_concil, "D:/romi_ofice/BI/TrabalhoNovoBI/list_table_concil.xlsx")
 
 
@@ -33,6 +36,8 @@ list_table_receb <- list.files(path = "D:/romi_ofice/BI/TrabalhoNovoBI/Recebidos
 Mes <- rep(c(04,08,12,02,01,07,06,05,03,11,10,09), each=37)
 # juntando o mes a tabela final
 list_table_receb <- cbind(list_table_receb, Mes )
+
+list_table_receb <- list_table_receb %>% select(-UF, -`Número do Orgão (Estatística)`, -`Data da última remessa`)
 
 write.xlsx(list_table_receb, "D:/romi_ofice/BI/TrabalhoNovoBI/list_table_receb.xlsx")
 
@@ -49,6 +54,8 @@ Mes <- rep(c(04,08,12,02,01,07,06,05,03,11,10,09), each=37)
 # juntando o mes a tabela final
 list_table_Soluci <- cbind(list_table_Soluci, Mes )
 
+list_table_Soluci <- list_table_Soluci %>% select(-UF)
+
 write.xlsx(list_table_Soluci, "D:/romi_ofice/BI/TrabalhoNovoBI/list_table_Soluci.xlsx")
 
 
@@ -63,5 +70,7 @@ list_table_Reclam <- list.files(path = "D:/romi_ofice/BI/TrabalhoNovoBI/Valores 
 Mes <- rep(c(04,08,12,02,01,07,06,05,03,11,10,09), each=37)
 # juntando o mes a tabela final
 list_table_Reclam <- cbind(list_table_Reclam, Mes )
+
+list_table_Reclam <- list_table_Reclam %>% select(-`Região Judiciária`)
 
 write.xlsx(list_table_Reclam, "D:/romi_ofice/BI/TrabalhoNovoBI/list_table_Reclam.xlsx")
